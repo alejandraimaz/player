b6f00406b0cd3a8bf55864eeeaf335d6ed1093dc
idlab <idlab@res11.intec.ugent.be>
2017-09-08 11:25:48 +0200

    First commit

--- samples/dash-if-reference-player/app/bundle.js
+++ samples/dash-if-reference-player/app/bundle.js
@@ -827,17 +827,15 @@ function listenToOrientation() {
 			viewport: utils.getSize(),
 			orientation: utils.getOrientation(),
 			originalEvent: ev
-			
 		});
 	}, intervals.orientation);
-	
+
 	window.addEventListener(eventType, handler);
 	listeners.orientation = {
 		eventType: eventType,
 		handler: handler
 	};
 }
-
 
 function listenToVisibility() {
 
@@ -916,7 +914,7 @@ function stopListeningTo(eventType) {
 
 module.exports = {
 	debug: function() {
-		 debug = true;
+		// debug = true;
 		utils.debug();
 	},
 	listenTo: listenTo,
@@ -1275,7 +1273,6 @@ class VideoAds {
 		this.adsCompleted = false;
 	}
 
-	
 	loadAdsLibrary() {
 		return new Promise((resolve, reject) => {
 			let googleSdkScript = document.querySelector('[src="//imasdk.googleapis.com/js/sdkloader/ima3.js"]');
@@ -2257,9 +2254,9 @@ class ThreeSixtyMedia {
 			this.camera.updateProjectionMatrix();
 		}
 		
-		
-		this.renderer.render(this.scene, this.camera);
+		this.renderer.render(this.scene, this.camera);	
 	}
+
 
 	render() {
 		this.renderer.clear();
@@ -2294,18 +2291,14 @@ class ThreeSixtyMedia {
 				this.renderer.setScissorTest( false );
 				this.renderer.setViewport( 0, 0, size.width, size.height );
 				this.vrDisplay.submitFrame(pose);
-
 			} else {
 				this.renderSceneView(pose, null);
-
 			}
 		} else {
 
 			// No VRDisplay found.
 			this.renderer.render(this.scene, this.camera);
-
 		}
-
 	}
 
 	destroy() {
@@ -2347,10 +2340,120 @@ class ThreeSixtyMedia {
 		this.buttonContainer.removeChild(el);
 	}
 }
-		
 module.exports = ThreeSixtyMedia;
 
 
+
+/***/ }),
+/* 15 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* jshint devel: true */
+const oUtils = __webpack_require__(16);
+
+let debug;
+
+function broadcast(eventType, data, target) {
+	target = target || document.body;
+
+	if (debug) {
+		console.log('o-viewport', eventType, data);
+	}
+
+	target.dispatchEvent(new CustomEvent('oViewport.' + eventType, {
+		detail: data,
+		bubbles: true
+	}));
+}
+
+function getHeight(ignoreScrollbars) {
+	return ignoreScrollbars ? document.documentElement.clientHeight : Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+
+function getWidth(ignoreScrollbars) {
+	return ignoreScrollbars ? document.documentElement.clientWidth : Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+}
+
+function getSize(ignoreScrollbars) {
+	return {
+		height: module.exports.getHeight(ignoreScrollbars),
+		width: module.exports.getWidth(ignoreScrollbars)
+	};
+}
+
+function getScrollPosition() {
+	const de = document.documentElement;
+	const db = document.body;
+
+	// adapted from https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+	const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+
+	const ieX = isCSS1Compat ? de.scrollLeft : db.scrollLeft;
+	const ieY = isCSS1Compat ? de.scrollTop : db.scrollTop;
+	return {
+		height: db.scrollHeight,
+		width: db.scrollWidth,
+		left: window.pageXOffset || window.scrollX || ieX,
+		top: window.pageYOffset || window.scrollY || ieY
+	};
+}
+
+function getOrientation() {
+	const orientation = window.screen.orientation || window.screen.mozOrientation || window.screen.msOrientation || undefined;
+	if (orientation) {
+		return typeof orientation === 'string' ?
+			orientation.split('-')[0] :
+			orientation.type.split('-')[0];
+	} else if (window.matchMedia) {
+		return window.matchMedia('(orientation: portrait)').matches ? 'portrait' : 'landscape';
+	} else {
+		return getHeight() >= getWidth() ? 'portrait' : 'landscape';
+	}
+}
+
+function detectVisiblityAPI() {
+	let hiddenName;
+	let eventType;
+	if (typeof document.hidden !== 'undefined') {
+		hiddenName = 'hidden';
+		eventType = 'visibilitychange';
+	} else if (typeof document.mozHidden !== 'undefined') {
+		hiddenName = 'mozHidden';
+		eventType = 'mozvisibilitychange';
+	} else if (typeof document.msHidden !== 'undefined') {
+		hiddenName = 'msHidden';
+		eventType = 'msvisibilitychange';
+	} else if (typeof document.webkitHidden !== 'undefined') {
+		hiddenName = 'webkitHidden';
+		eventType = 'webkitvisibilitychange';
+	}
+
+	return {
+		hiddenName: hiddenName,
+		eventType: eventType
+	};
+}
+
+function getVisibility() {
+	const hiddenName = detectVisiblityAPI().hiddenName;
+	return document[hiddenName];
+}
+
+module.exports = {
+	debug: function() {
+		debug = true;
+	},
+	broadcast: broadcast,
+	getWidth: getWidth,
+	getHeight: getHeight,
+	getSize: getSize,
+	getScrollPosition: getScrollPosition,
+	getVisibility: getVisibility,
+	getOrientation: getOrientation,
+	detectVisiblityAPI: detectVisiblityAPI,
+	debounce: oUtils.debounce,
+	throttle: oUtils.throttle
+};
 
 
 /***/ }),
@@ -2845,6 +2948,7 @@ function isObject(arg) {
 function isUndefined(arg) {
   return arg === void 0;
 }
+
 
 /***/ })
 /******/ ]);
